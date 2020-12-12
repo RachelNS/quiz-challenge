b5acd8f82a158ca353a9dd498993f51d0459122c
RachelNS <rachelnelsonschille@gmail.com>
2020-12-12 11:11:01 -0800

    pseudocode for js file

---
+++ script.js
@@ -0,0 +1,14 @@
+// TODO: Timer counts down 60 seconds, stops at zero
+
+// TODO: When user clicks "start quiz" button:
+    // TODO: Timer starts counting down
+    // TODO: Intro paragraph goes away, first question appears
+
+    // TODO: While loop--while timer is not zero and there are more questions, these things happen
+            // TODO: Questions disappear when they are answered, and are replaced with the next question. Each question is an unordered list with possible answers.
+            // TODO: If they click the correct answer, their score goes up. 
+            // TODO: If they answer incorrectly, deduct 10 seconds from the timer.
+
+    // TODO: When all questions are answered or the timer runs out, display "game over" message with score
+        // TODO: Collect user initials and score, store in local storage
+        // TODO: If they click "view high scores" button, retrieve user data from local storage and display
