e0f548a1cfc3926f829400b1eb1e7561c28c3d44
RachelNS <rachelnelsonschille@gmail.com>
2020-12-13 21:42:04 -0800

    created array of objects with questions, stuck on how to access them

--- script.js
+++ script.js
@@ -17,64 +17,70 @@ var questionText = document.querySelector("#question-text")
 var chocieList = document.querySelector("#choices");
 
 // Time displayed in timeLeft @ start of game
-var startTime = 60;
+var startTime = 5;
 
 // Score @ beginning of game (not displayed)
 var score = 0;
 
-// First question in the quiz
-var firstQuestion = {
-    question: "All old people know each other! Don't you know that?",
-    answer1: "Master Pakku",
-    answer2: "King Bumi",
-    answer3: "Admiral Zhao",
-    answer4: "Uncle Iroh",
-}
+// This lays out quiz questions and their answers, indicating which are correct
+var quiz = [
+    {
+        question1: "All old people know each other! Don't you know that?",
+        answers1: [
+            { text: "Master Pakku", correct: false },
+            { text: "King Bumi", correct: true },
+            { text: "Admiral Zhao", correct: false },
+            { text: "Uncle Iroh", correct: false },
+        ]
+    },
+    {
+        question2: "Hey! We're in enemy territory. Those are enemy birds.",
+        answers2: [
+            { text: "Zuko", correct: false },
+            { text: "Toph", correct: false },
+            { text: "Mai", correct: false },
+            { text: "Sokka", correct: false },
+        ]
+    },
+    {
+        question3: "Very well. But first I must finish my roast duck.",
+        answers3: [
+            { text: "Uncle Iroh", correct: true },
+            { text: "Jeong Jeong", correct: false },
+            { text: "Monk Gyatso", correct: false },
+            { text: "Master Piandao", correct: false },
+        ]
+    },
+    {
+        question4: "My own mother thought I was a monster. She was right, of course, but it still hurt.",
+        answers4: [
+            { text: "Sokka", correct: false },
+            { text: "Haru", correct: false },
+            { text: "Azula", correct: true },
+            { text: "Jet", correct: false },
+        ]
+    },
+    {
+        question5: "You've seen nothing once, you've seen it a thousand times.",
+        answers5: [
+            { text: "Zuko", correct: false },
+            { text: "Toph", correct: true },
+            { text: "Suki", correct: false },
+            { text: "Katara", correct: false },
+        ]
+    },
+    {
+        question6: "My cabbages!",
+        answers6: [
+            { text: "Come on now", correct: false },
+            { text: "You know this", correct: false },
+            { text: "Seriously, this one's a gimmie", correct: false },
+            { text: "Cabbage vendor", correct: true },
+        ]
+    }
+]
 
-// Second question in the quiz
-var secondQuestion = {
-    question: "Hey! We're in enemy territory. Those are enemy birds.",
-    answer1: "Zuko",
-    answer2: "Toph",
-    answer3: "Mai",
-    answer4: "Sokka",
-}
-
-// Third question in the quiz
-var thirdQuestion = {
-    question: "Very well. But first I must finish my roast duck.",
-    answer1: "Uncle Iroh",
-    answer2: "Jeong Jeong",
-    answer3: "Monk Gyatso",
-    answer4: "Master Piandao",
-}
-
-// Fourth question in the quiz
-var fourthQuestion = {
-    question: "My own mother thought I was a monster. She was right, of course, but it still hurt.",
-    answer1: "Sokka",
-    answer2: "Haru",
-    answer3: "Azula",
-    answer4: "Jet",
-}
-
-// Fifth question in the quiz
-var fifthQuestion = {
-    question: "You've seen nothing once, you've seen it a thousand times.",
-    answer1: "Zuko",
-    answer2: "Toph",
-    answer3: "Suki",
-    answer4: "Katara",
-}
-
-// Sixth question in the quiz
-var sixthQuestion = {
-    question: "My cabbages!",
-    answer1: "Come on now",
-    answer2: "Keep looking",
-    answer3: "Seriously, this one's a gimmie",
-    answer4: "Cabbage vendor",
-}
+console.log(quiz);
 
 // Timer counts down 60 seconds, stops at zero
 function countDown() {
@@ -84,36 +90,18 @@ function countDown() {
         if (startTime === 0) {
             clearInterval(timeInterval);
             timeLeft.textContent = "Time's up!"
-    
+
         }
 
     }, 1000);
 }
+
 // When user clicks "start quiz" button, countdown begins and intro paragraph disappears
 startButton.addEventListener("click", function () {
     countDown();
     introText.textContent = "";
     titleText.textContent = "";
-    startButton.style.display = "none";
-
-    // Populates h2 tag with question text, creates li tags to hold answer text
-    questionText.textContent = firstQuestion.question;
-    var firstAnswer = document.createElement("li");
-    firstAnswer.textContent = firstQuestion.answer1;
-    chocieList.appendChild(firstAnswer);
-
-    var secondAnswer = document.createElement("li");
-    secondAnswer.textContent = firstQuestion.answer2;
-    chocieList.appendChild(secondAnswer);
-
-    var thirdAnswer = document.createElement("li");
-    thirdAnswer.textContent = firstQuestion.answer3;
-    chocieList.appendChild(thirdAnswer);
-
-    var fourthAnswer = document.createElement("li");
-    fourthAnswer.textContent = firstQuestion.answer4;
-    chocieList.appendChild(fourthAnswer);
-
+    startButton.style.display = "none";   
 
 })
 
@@ -138,3 +126,6 @@ startButton.addEventListener("click", function () {
     // TODO: When all questions are answered or the timer runs out, display "game over" message with score
         // TODO: Collect user initials and score, store in local storage. If score is greater than 3, display "Flame-O, hotman! Brag to your friends!" If score is lower than 3, display "That's rough, buddy. Tell everyone how much you suck."
         // TODO: If they click "view high scores" button, retrieve user data from local storage and display
+
+
+
