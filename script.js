811cafaf1bfae4c89f69540f609efd8ba0777e75
RachelNS <rachelnelsonschille@gmail.com>
2020-12-15 09:50:08 -0800

    added score display and submission form, as well as storing variables in local storage

--- script.js
+++ script.js
@@ -16,11 +16,13 @@ var questionText = document.querySelector("#question-text")
 // This is the div containing the questions
 var chocieList = document.querySelector("#choices");
 
+
+
 // Time displayed in timeLeft @ start of game
-var startTime = 60;
+var startTime = 5;
 
 // Score @ beginning of game (not displayed)
-var score = 0;
+var score = 5;
 
 // This lays out quiz questions and their answers, indicating which are correct
 var quiz = [
@@ -67,7 +69,6 @@ function countDown() {
             timeLeft.textContent = "Time's up!"
 
         }
-
     }, 1000);
 }
 
@@ -77,54 +78,96 @@ startButton.addEventListener("click", function () {
     introText.textContent = "";
     titleText.textContent = "";
     startButton.style.display = "none";
-    questionText.textContent = quiz[0].question;
-
-    // TODO: For each iteration of "quiz:"
-    // -Change questionText (h2) content to "quiz[i]"
-    // -Change answerList (li) text to quiz[i].answers[j]
-    // 
-
-
-
-    for (var i = 0; i < quiz[0].answers.length; i++) {
-        var answerList = document.createElement("li");
-        answerList.textContent = quiz[0].answers[i];
-        chocieList.appendChild(answerList);  
-    }
-
-    chocieList.addEventListener("click", function (event){
-        if(event.target.textContent===quiz[0].correctAnswer) {
-            score++;
-            console.log(score);
-        }
-    })
 
     
+    // For each index in the quiz array, run this block of code
+    quiz.forEach(function(quiz){
+        // Set h2 tag to current question text
+        questionText = quiz.question;
+        // Loop over each answers array and create a new tag to hold the text of each answer
+        for(i=0; i<quiz.answers.length; i++) {
+            var answerList = document.createElement("li"); 
+            answerList.textContent = quiz.answers[i];
+            chocieList.appendChild(answerList);
+        }
+    });
 
+    // // Loop over quiz array displaying each question in the h2 tag
+    // for (var i = 0; i < quiz.length; i++) {
+    //     questionText = quiz[i].question;
+    //     console.log(questionText);
+    // }
 
-
-
-
+    // // Loop over the answers for each question and create a new li tag to hold each answer
+    // for (var j = 0; j < quiz[j].answers.length; j++) {
+    //     var answerList = document.createElement("li");
+    //     answerList.textContent = quiz[j].answers[j];
+    //     chocieList.appendChild(answerList);
+    // }
 
 })
+
+
+
+// Tracking variable for while loop
+// var i = 0
+
+// As long as there is still time on the clock and the quiz has not run out of questions, run the following block of code
+// while (timeLeft !== 0 && i < quiz.length) {
+
+//     // For each 
+//     for (var j = 0; j < quiz[i].answers.length; j++) {
+//         questionText.textContent = quiz[i].question;
+
+//         var answerList = document.createElement("li");
+//         answerList.textContent = quiz[i].answers[j];
+//         chocieList.appendChild(answerList);
+
+//         chocieList.addEventListener("click", function (event) {
+//             if (event.target.textContent === quiz[i].correctAnswer) {
+//                 score++;
+//                 console.log(score);
+//             }
+//             else {
+//                 timeLeft = timeLeft - 10;
+//             }
+//             questionText.textContent = "";
+//             answerList.textContent = "";
+
+
+//         });
+//         i++;
+//     }
+// }
+
+
+        // for (var i=0; i<quiz.length; i++) {
+
+        // }
+
+        // for (var i = 0; i < quiz[0].answers.length; i++) {
+        //     var answerList = document.createElement("li");
+        //     answerList.textContent = quiz[0].answers[i];
+        //     chocieList.appendChild(answerList);
+        // }
+
+        // chocieList.addEventListener("click", function (event) {
+        //     if (event.target.textContent === quiz[0].correctAnswer) {
+        //         score++;
+        //         console.log(score);
+        //     }
+        //     else {
+        //         timeLeft = timeLeft - 10;
+        //     }
+
+        // })
+
 
 
 //     // TODO: for loop going over quiz array, generate an li element for each answer. If the answer is correct, blah blah blah if the answer is wrong blah blah blah
 //     // TODO: seperate html for score page?
 
 // })
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     // TODO: While loop--while timer is not zero and there are more questions, these things happen
@@ -135,3 +178,61 @@ startButton.addEventListener("click", function () {
     // TODO: When all questions are answered or the timer runs out, display "game over" message with score
         // TODO: Collect user initials and score, store in local storage. If score is greater than 3, display "Flame-O, hotman! Brag to your friends!" If score is lower than 3, display "That's rough, buddy. Tell everyone how much you suck."
         // TODO: If they click "view high scores" button, retrieve user data from local storage and display
+
+
+// This is the form users will use to submit their initials and score
+var userStats = document.querySelector("#user-stats")
+
+// This is where the user's score will be displayed at the end of the game
+var userScore = document.querySelector("#score-display")
+
+// This is the input field for user initials
+var userInitials = document.querySelector("#initials-input");
+
+// This is the button for users to submit their stats
+var submitButton = document.querySelector("#submit");
+
+// This is the h3 tag displaying the results of the quiz
+var userResults = document.querySelector("#results");
+
+// This is the button to display previous user scores
+var highScores = document.querySelector("#highscores");
+
+// This variable is used to store the last user's data in local storage
+userScore.value = score;
+var user = {
+    initials: userInitials.value,
+    userScore: score   
+};
+
+if(score>=3) {
+    userResults.textContent = "Flame-o, Hotman! Brag to your friends!"
+}
+else{
+    userResults.textContent = "That's rough, buddy. Tell everyone how much you suck."
+}
+
+// When user submits their data, it will be collected and stored in local storage
+submitButton.addEventListener("click", function(event){
+    event.preventDefault();
+    if(userInitials.value==="") {
+        userResults.textContent = "Please enter your initials!"
+    }
+    else{
+        userResults.textContent = "Success! Everyone knows your score now!"
+        // Send user data to local storage
+        localStorage.setItem("user", JSON.stringify(user));
+    }
+})
+
+// Retrieve last user data from local storage
+highScores.addEventListener("click", function(){
+userResults.textContent = "Here ya go, nosy!";
+var lastHighScore = localStorage.getItem("user", JSON.parse(user));
+console.log (lastHighScore);
+userInitials.value = lastHighScore.initials;
+userScore.value = lastHighScore.score;
+})
+
+
+
